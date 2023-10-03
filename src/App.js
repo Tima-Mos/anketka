1dfcdf3ad57af0ef0e6e9d77bf848415a31f78e4
Tima-Mos <timmos2011@gmail.com>
2023-10-03 13:40:32 +0400

    axios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/Convert.css'
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import Convert from "./components/Convert";
+import axios from "axios";
 function App() {
     const [posts, setPosts] = useState([
         {id:1, title:'клоп', body:'писечки'},
@@ -15,6 +16,11 @@ function App() {
         {id:2, name:'EUR'},
         {id:3, name:'USD'},
     ])
+    const [resp, setResp] = useState();
+    async function changeCurency(){
+        const responce = await axios.get('')
+        console.log(responce.data)
+    }
 
   return (
     <div className="App">
